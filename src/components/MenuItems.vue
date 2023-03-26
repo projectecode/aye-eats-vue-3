e238a508fd2053774ce5ac1af6ea0a47711dcecd
Aditya Ekawade <78055556+projectecode@users.noreply.github.com>
2023-03-26 13:04:59 -0700

    fetch data

---
+++ src/components/MenuItems.vue
@@ -0,0 +1,43 @@
+<template>
+  <q-page-container>
+    <q-page padding>
+      <div class="q-pa-md">
+        <div class="row q-col-gutter-md">
+          <div v-for="item in menu" :key="item.id" class="col-3">
+            <q-card>
+              <q-img :src="item.img" />
+              <q-card-section>
+                <div class="text-h6">{{ item.title }}</div>
+                <q-chip outline color="teal" text-color="white">
+                  {{ item.category }}
+                </q-chip>
+              </q-card-section>
+            </q-card>
+          </div>
+        </div>
+      </div>
+    </q-page>
+  </q-page-container>
+</template>
+
+<script>
+export default {
+  name: "menu",
+  data() {
+    return {
+      menu: [],
+    };
+  },
+  created() {
+    console.log("created!");
+    fetch("https://my-json-server.typicode.com/projectecode/demo/menu")
+      .then((res) => res.json())
+      .then((json) => {
+        this.menu = json;
+        console.log("this.menu", this.menu);
+      });
+  },
+};
+</script>
+
+<style></style>
